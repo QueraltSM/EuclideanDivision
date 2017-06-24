51e33c634311474571d32ebf3253454f0b0c0b1e
QueraltSosaMompel <queraltsosa@hotmail.com>
2017-06-24 20:14:57 +0100

    Updates

--- .idea/EulideanDivision.py
+++ .idea/EulideanDivision.py
@@ -8,6 +8,19 @@ def MCD_Euclides(a, b):
 
 
 if __name__ == "__main__":
-    print(MCD_Euclides(60, 48))
-    print(MCD_Euclides(3768, 1727))
-    print(MCD_Euclides(25, 5))
+    print("Test 1: " + str(MCD_Euclides(60, 48)) + "\n")
+    print("Test 2: " + str(MCD_Euclides(3768, 1727)) + "\n")
+    print("Test 3 : " + str(MCD_Euclides(25, 5)))
+
+    """
+    Console results:
+
+    Test 1: 12
+
+    Test 2: 157
+
+    Test 3 : 5
+
+    Process finished with exit code 0
+
+    """
